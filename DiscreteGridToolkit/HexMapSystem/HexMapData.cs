9becf19b8e6089ec97a647e3f52a62664aebe018
MycroftCooper <227108696@qq.com>
2022-04-13 22:53:37 +0800

    移除毕设暂存

--- DiscreteGridToolkit/HexMapSystem/HexMapData.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class HexMapData {
-    public Dictionary<Vector3, HexBlockCtrl> Map;
-    public Vector2Int MapSize;
-
-}
