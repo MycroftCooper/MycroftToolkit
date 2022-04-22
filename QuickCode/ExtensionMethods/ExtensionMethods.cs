9e189f2a631c5fe3012edc3e0d25b83f60333b90
FengAnMeng <fenganmeng0326@163.com>
2022-04-22 18:35:37 +0800

    [功能] 增加字符串扩展方法

--- QuickCode/ExtensionMethods/ExtensionMethods.cs
+++ QuickCode/ExtensionMethods/ExtensionMethods.cs
@@ -1,53 +1,55 @@
 ﻿using UnityEngine;
 using UnityEngine.UI;
 
-public static class ExtensionMethods {
-    public static void RemoveComponent<Component>(this GameObject obj, bool immediate = false) {
-        Component component = obj.GetComponent<Component>();
-        if (component != null) {
-            if (immediate) {
-                Object.DestroyImmediate(component as Object, true);
-            } else {
-                Object.Destroy(component as Object);
+namespace MycroftToolkit.QuickCode {
+    public static class ExtensionMethods {
+        public static void RemoveComponent<Component>(this GameObject obj, bool immediate = false) {
+            Component component = obj.GetComponent<Component>();
+            if (component != null) {
+                if (immediate) {
+                    Object.DestroyImmediate(component as Object, true);
+                } else {
+                    Object.Destroy(component as Object);
+                }
             }
         }
-    }
 
-    public static void LocalRest(this Transform tr) {
-        tr.localPosition = Vector3.zero;
-        tr.localRotation = Quaternion.identity;
-        tr.localScale = Vector3.one;
-    }
-    public static void Reset(this Transform tr) {
-        tr.position = Vector3.zero;
-        tr.rotation = Quaternion.identity;
-        tr.localScale = Vector3.zero;
-    }
+        public static void LocalRest(this Transform tr) {
+            tr.localPosition = Vector3.zero;
+            tr.localRotation = Quaternion.identity;
+            tr.localScale = Vector3.one;
+        }
+        public static void Reset(this Transform tr) {
+            tr.position = Vector3.zero;
+            tr.rotation = Quaternion.identity;
+            tr.localScale = Vector3.zero;
+        }
 
-    /// <summary>
-    /// 只设置颜色不改变alpha
-    /// </summary>
-    public static void SetColor(this Image image, Color color) {
-        Color tempColor = color;
-        tempColor.a = image.color.a;
-        image.color = tempColor;
-    }
+        /// <summary>
+        /// 只设置颜色不改变alpha
+        /// </summary>
+        public static void SetColor(this Image image, Color color) {
+            Color tempColor = color;
+            tempColor.a = image.color.a;
+            image.color = tempColor;
+        }
 
-    /// <summary>
-    /// 只设置alpha不改变颜色
-    /// </summary>
-    public static void SetAlpha(this Image image, float alpha) {
-        Color tempColor = image.color;
-        tempColor.a = alpha;
-        image.color = tempColor;
-    }
+        /// <summary>
+        /// 只设置alpha不改变颜色
+        /// </summary>
+        public static void SetAlpha(this Image image, float alpha) {
+            Color tempColor = image.color;
+            tempColor.a = alpha;
+            image.color = tempColor;
+        }
 
-    /// <summary>
-    /// 只设置alpha不改变颜色
-    /// </summary>
-    public static void SetAlpha(this SpriteRenderer spriteRenderer, float alpha) {
-        Color tempColor = spriteRenderer.color;
-        tempColor.a = alpha;
-        spriteRenderer.color = tempColor;
+        /// <summary>
+        /// 只设置alpha不改变颜色
+        /// </summary>
+        public static void SetAlpha(this SpriteRenderer spriteRenderer, float alpha) {
+            Color tempColor = spriteRenderer.color;
+            tempColor.a = alpha;
+            spriteRenderer.color = tempColor;
+        }
     }
 }
---
+++ QuickCode/ExtensionMethods/StringExtensions.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Globalization;
+using System.Text;
+
+namespace MycroftToolkit.QuickCode {
+    //
+    // 摘要:
+    //     String method extensions.
+    public static class StringExtensions {
+        //
+        // 摘要:
+        //     Eg MY_INT_VALUE => MyIntValue
+        public static string ToTitleCase(this string input) {
+            StringBuilder stringBuilder = new StringBuilder();
+            for (int i = 0; i < input.Length; i++) {
+                char c = input[i];
+                if (c == '_' && i + 1 < input.Length) {
+                    char c2 = input[i + 1];
+                    if (char.IsLower(c2)) {
+                        c2 = char.ToUpper(c2, CultureInfo.InvariantCulture);
+                    }
+
+                    stringBuilder.Append(c2);
+                    i++;
+                } else {
+                    stringBuilder.Append(c);
+                }
+            }
+
+            return stringBuilder.ToString();
+        }
+
+        //
+        // 摘要:
+        //     Returns whether or not the specified string is contained with this string
+        public static bool Contains(this string source, string toCheck, StringComparison comparisonType) {
+            return source.IndexOf(toCheck, comparisonType) >= 0;
+        }
+
+        //
+        // 摘要:
+        //     Ex: "thisIsCamelCase" -> "This Is Camel Case"
+        public static string SplitPascalCase(this string input) {
+            if (input == null || input.Length == 0) {
+                return input;
+            }
+
+            StringBuilder stringBuilder = new StringBuilder(input.Length);
+            if (char.IsLetter(input[0])) {
+                stringBuilder.Append(char.ToUpper(input[0]));
+            } else {
+                stringBuilder.Append(input[0]);
+            }
+
+            for (int i = 1; i < input.Length; i++) {
+                char c = input[i];
+                if (char.IsUpper(c) && !char.IsUpper(input[i - 1])) {
+                    stringBuilder.Append(' ');
+                }
+
+                stringBuilder.Append(c);
+            }
+
+            return stringBuilder.ToString();
+        }
+    }
+}
