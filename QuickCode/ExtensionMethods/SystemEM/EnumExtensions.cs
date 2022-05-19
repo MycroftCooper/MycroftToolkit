937719ea5b9c16645053b14df8d65a5a853433ce
FengAnMeng <fenganmeng0326@163.com>
2022-05-19 10:13:44 +0800

    [整理] 整理扩展方法目录

---
+++ QuickCode/ExtensionMethods/SystemEM/EnumExtensions.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+public static class EnumExtension {
+
+    public static T ToEnum<T>(this int value)
+        => (T)Enum.ToObject(typeof(T), value);
+    public static List<Enum> GetFlags(this Enum enumValue) {
+        List<Enum> output = new List<Enum>();
+        foreach (Enum t in Enum.GetValues(enumValue.GetType())) {
+            if (enumValue.HasFlag(t)) output.Add(t);
+        }
+        return output;
+    }
+}
