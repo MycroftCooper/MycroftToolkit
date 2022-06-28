e1a1c94db5fa80be9a98fe3b02bfb0131aa278d0
FengAnMeng <fenganmeng0326@163.com>
2022-06-28 12:05:12 +0800

    [功能] 增加获取枚举类所有枚举项的扩展函数

--- QuickCode/ExtensionMethods/SystemEM/EnumExtensions.cs
+++ QuickCode/ExtensionMethods/SystemEM/EnumExtensions.cs
@@ -1,15 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 public static class EnumExtension {
 
     public static T ToEnum<T>(this int value)
         => (T)Enum.ToObject(typeof(T), value);
     public static List<Enum> GetFlags(this Enum enumValue) {
+        if (enumValue == null) return null;
         List<Enum> output = new List<Enum>();
         foreach (Enum t in Enum.GetValues(enumValue.GetType())) {
             if (enumValue.HasFlag(t)) output.Add(t);
         }
+        return output;
+    }
+
+    public static List<T> GetEnums<T>(Type enumType) where T : Enum {
+        if (enumType == null) return null;
+        var enumArray = Enum.GetValues(enumType);
+        List<T> output = enumArray.OfType<T>().ToList();
         return output;
     }
 }
