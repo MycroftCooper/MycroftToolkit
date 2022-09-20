f49beebcc68fe8c30ab62713abb01e60b4d41258
FengAnMeng <fenganmeng0326@163.com>
2022-09-20 11:59:54 +0800

    [功能] AA工具包

---
+++ QuickResource/AATools/AAUtility.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.AddressableAssets;
+using UnityEngine.ResourceManagement.AsyncOperations;
+
+public static class AAUtility {
+    public static string GetAddress(this AssetReference reference) {
+        var loadResourceLocations = Addressables.LoadResourceLocationsAsync(reference);
+        var result = loadResourceLocations.WaitForCompletion();
+        string key = string.Empty;
+        if (result.Count > 0) {
+            key = result[0].PrimaryKey;
+        }
+        Addressables.Release(loadResourceLocations);
+        return key;
+    }
+
+    public static AsyncOperationHandle<T> LoadAA<T>(string assetName, Action<AsyncOperationHandle<T>> callBack) {
+        AsyncOperationHandle<T> output = Addressables.LoadAssetAsync<T>(assetName);
+        output.Completed += callBack;
+        return output;
+    }
+}
---
+++ QuickResource/AATools/Editor/AAScanner.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+namespace MycroftToolkit.QuickResource.AATools {
+    public class AAScanner {
+
+    }
+}
