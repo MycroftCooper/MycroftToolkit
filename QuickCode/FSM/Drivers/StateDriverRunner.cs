ba8d6a237b9b7381799cbf5792323db702c74dd5
烤麸 <2570150161@qq.com>
2023-02-06 09:15:45 +0800

    [优化] 移动部分功能到急急框架

--- QuickCode/FSM/Drivers/StateDriverRunner.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace MycroftToolkit.QuickTool.FSM {
-    public class StateDriverRunner {
-        public StateEvent FixedUpdate;
-        public StateEvent Update;
-        public StateEvent LateUpdate;
-    }
-}
