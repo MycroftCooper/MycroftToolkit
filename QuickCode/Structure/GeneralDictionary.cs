5b30f40755752a76adc3e74033a86576deab1423
MycroftCooper <2570150161@qq.com>
2024-11-22 00:29:22 +0800

    添加数据结构目录

---
+++ QuickCode/Structure/GeneralDictionary.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace  MycroftToolkit.QuickCode {
+    public class GeneralDictionary<TKey> where TKey : notnull {
+        public readonly Dictionary<TKey, object> Dict;
+        public int Count => Dict.Count;
+        public GeneralDictionary() => Dict = new Dictionary<TKey, object>();
+        public void Add(TKey key, object value) => Dict.Add(key, value);
+        public TValue Get<TValue>(TKey key) {
+            if (Dict.TryGetValue(key, out var output)) {
+                return (TValue)output;
+            }
+            Debug.LogError($"字典中未找到键{key}");
+            return default;
+        }
+        public bool Set(TKey key, object value, bool forceAdd = false) {
+            if (!Dict.ContainsKey(key)) {
+                if (!forceAdd) {
+                    return false;
+                }
+                Add(key, value);
+            }
+            Dict[key] = value;
+            return true;
+        }
+        public bool ContainsKey(TKey key) => Dict.ContainsKey(key);
+        public bool ContainsValue(object value) => Dict.ContainsValue(value);
+        public bool Remove(TKey key) => Dict.Remove(key);
+        public void Clear() => Dict.Clear();
+    }
+}
