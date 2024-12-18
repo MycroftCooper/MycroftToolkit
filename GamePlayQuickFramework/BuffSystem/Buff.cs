151d0214f94a9f282b61cb5735a647f88bd69d1a
MycroftCooper <2570150161@qq.com>
2024-12-18 21:15:13 +0800

    为啥又得写一次buff呢，难受啊

--- GamePlayQuickFramework/BuffSystem/Buff.cs
+++ GamePlayQuickFramework/BuffSystem/Buff.cs
@@ -1,10 +1,31 @@
-﻿namespace BuffSystem {
+﻿using System;
+using UnityEngine;
+
+namespace BuffSystem {
     public class Buff {
+        public string Name;
+        public bool HasOwner => string.IsNullOrEmpty(Owner);
+        public string Owner;
+        public EffectTypes EffectType;
+        public OverlayRules OverlayRules;
+        public TriggerRules TriggerRules;
+        public RemoveRules RemoveRules;
         
+        public Action<Buff> OnAdded;
+        public Action<Buff> OnRemoved;
+
+        public void OnAddBuffHandler(string target) {
+            if (HasOwner) {
+                Debug.LogError($"Buff {Name} is already owned by {Owner}!");
+                return;
+            }
+            Owner = target;
+            OnAdded?.Invoke(this);
+        }
     }
 
     public enum EffectTypes { Positive, Neutral, Negative }
-    public enum OverlayRules { AddTime, Overlay, Invalid }
-    public enum TriggerRules { ByTimer, ByTicker, OnAdd, OnRemove }
-    public enum RemoveRules { ByTimer, ByTicker  }
+    public enum OverlayRules { AddTime, Stack, Replace, Reset, NotAllowed }
+    public enum TriggerRules { Manual, ByTimer, ByTicker, ByEvent, OnAdd, OnRemove }
+    public enum RemoveRules { Manual, ByTimer, ByTicker, ByEvent }
 }
---
+++ GamePlayQuickFramework/BuffSystem/BuffFactory.cs
@@ -0,0 +1,5 @@
+namespace BuffSystem {
+    public class BuffFactory {
+        
+    }
+}
