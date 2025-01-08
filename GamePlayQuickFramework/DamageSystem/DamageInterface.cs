28efc58ae177bd1261ff9a4eca9def13c0ee3406
MycroftCooper <2570150161@qq.com>
2025-01-08 12:18:51 +0800

    DamageSystem

--- GamePlayQuickFramework/DamageSystem/DamageInterface.cs
+++ GamePlayQuickFramework/DamageSystem/DamageInterface.cs
@@ -1,4 +1,9 @@
 namespace MycroftToolkit.GamePlayQuickFramework.DamageSystem {
+    public interface IDamageable {
+        public bool IsDamageable { get; }
+        public void TakeDamage(DamageData damageData);
+    }
+    
     public interface IDamageTrigger {
         public void TriggerDamage(IDamageable target);
     }
--- GamePlayQuickFramework/DamageSystem/IDamageable.cs
+++
@@ -1,6 +0,0 @@
-namespace MycroftToolkit.GamePlayQuickFramework.DamageSystem {
-    public interface IDamageable {
-        public bool IsDamageable { get; }
-        public void TakeDamage(DamageData damageData);
-    }
-}
